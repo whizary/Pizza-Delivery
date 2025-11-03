af30e880eb93034df46066c1f11f9f4bdd91ef83
GorillaElliot <eln0409@student.skelleftea.se>
2025-11-03 18:31:12 +0100

    Update movement.gd
    Dodge fix och dodge cooldown och sortering av variabler, movement och dodge mechanics

--- movement.gd
+++ movement.gd
@@ -4,32 +4,54 @@ extends CharacterBody2D
 
 var walk_speed = 150
 var gravity = 0
+var dodgeCD = 2
+var dodgeBool = true
+
+func _process(delta):
+	dodgeCD -= delta
+	if dodgeCD <=0 && dodgeBool == false:
+		dodgeBool = true
 
 func get_input():
 	velocity.x = 0
+	velocity.y = 0
+	
+	#VARIABLES
 	var right = Input.is_action_pressed('move_right')	
 	var left = Input.is_action_pressed('move_left')
 	
-	velocity.y = 0
 	var back = Input.is_action_pressed('move_up')
 	var forward = Input.is_action_pressed('move_down')
 	
 	var run = Input.is_key_pressed(KEY_SHIFT)
 	var dodge = Input.is_action_just_pressed("KEY_Q")
 	
+	#DODGE MECHANICS
+	if dodge && right && dodgeBool == true:
+		position.x += 100
+		dodgeBool = false
+		dodgeCD = 2
+		
+	elif dodge && left && dodgeBool == true:
+		position.x -= 100
+		dodgeBool = false
+		dodgeCD = 2
+		
+	elif dodge && back && dodgeBool == true:
+		position.y -= 100
+		dodgeBool = false
+		dodgeCD = 2
+		
+	elif dodge && forward && dodgeBool == true:
+		position.y += 100
+		dodgeBool = false
+		dodgeCD = 2
+	
+	#MOVEMENT MECHANICS
 	if run:
 		walk_speed = 200
 	elif right || left || back || forward || right && forward || right && back || left && back || left && forward:
 		walk_speed = 150
-	
-	if dodge && right:
-		position.x += 100
-	elif dodge && left:
-		position.x -= 100
-	elif dodge && back:
-		position.y -= 100
-	elif dodge && forward:
-		position.y += 100
 	
 	if right:
 		velocity.x += walk_speed
@@ -65,7 +87,6 @@ func get_input():
 	elif left && forward:
 		velocity.x = -walk_speed * 0.7
 		velocity.y = walk_speed * 0.7
-
 
 func _physics_process(delta):
 	velocity.y += gravity * delta
